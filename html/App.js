1d7ae04a6467b799da5396ccd2b55cda5994a257
dilyans <dkupenov@gmail.com>
2019-10-24 22:36:44 -0700

    add timer to server to send websocket messages to the client + add missing files

---
+++ html/App.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import './App.css';
+import {States, Devices, Sensors, Messages} from './components/defines';
+import SensorDisplay from "./components/sensordisplay";
+import DeviceControl from './components/control'
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            temperature: {},
+            humidity: {},
+            historicData: []
+        };
+        // init device states
+        for (const d of Devices.enumValues) {
+            this.state[d.stateName] = States.OFF;
+        }
+        // init sensor states
+        for (const s of Sensors.enumValues) {
+            this.state[s.valueName] = s.defaultValue;
+        }
+        this.handleDeviceUpdate = this.handleDeviceUpdate.bind(this);
+        this.wsConnection = undefined;
+    }
+
+    componentDidMount() {
+
+        const wsUri = 'ws://localhost:8080/ws';
+        const conn = new WebSocket(wsUri);
+        this.wsConnection = conn;
+        conn.onopen =  () => {
+            console.log('Connected.');
+            this.sendWebsocketCmd(Messages.HISTORY,{});
+        };
+        conn.onmessage = e => {
+            console.log('New message ' + e.data);
+            this.handleServerPush(e.data);
+        };
+    }
+
+    handleServerPush(e) {
+        const msg = JSON.parse(e);
+        const msgType = Messages.enumValueOf(msg['cmd']);
+        if (msgType === Messages.SENSOR_UPDATE) {
+            let newState = {};
+            Sensors.enumValues.forEach(s => {
+                    newState[s.valueName] = msg['payload'][s.valueName];
+                }
+            )
+            this.setState(newState);
+        }
+        if (msgType === Messages.DEVICE_UPDATE) {
+            let newState = {};
+            Devices.enumValues.forEach( d => {
+                if(msg['payload'][d.stateName]) {
+                    newState[d.stateName] =
+                        States.enumValueOf(msg['payload'][d.stateName]);
+                }
+            });
+            this.setState(newState);
+        }
+        if (msgType === Messages.HISTORY) {
+            this.setState({'history': msg['payload']});
+        }
+    }
+
+    handleDeviceUpdate(value, device) {
+        console.log("Got: " + value.toString() + " " + device);
+        let newState = {};
+        newState[device.stateName] = value;
+        this.setState(newState);
+        const payload = {};
+        payload[device.stateName] = value.name.toString();
+        this.sendWebsocketCmd(Messages.DEVICE_UPDATE, payload);
+    }
+
+    sendWebsocketCmd(cmd, payload){
+        const newState = {};
+        newState['cmd']=cmd.name;
+        newState['payload'] = payload;
+        const msg = JSON.stringify(newState);
+        this.wsConnection.send(msg);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div>{
+                    Sensors.enumValues.map(sensor =>
+                        <SensorDisplay
+                            key={sensor.name}
+                            name={sensor.label}
+                            value={this.state[sensor.valueName]}/>)
+                }
+                </div>
+                <div>
+                    {
+                        Devices.enumValues.map(device =>
+                            <DeviceControl
+                                key={device.name}
+                                device={device}
+                                state={this.state[device.stateName]}
+                                onDeviceUpdate={this.handleDeviceUpdate}/>
+                        )
+                    }
+                </div>
+            </div>
+        )
+    };
+}
+
+export default App;
--- server/server.py
+++ server/server.py
@@ -2,10 +2,20 @@ import logging
 import aiohttp
 from aiohttp import web
 import json
+import asyncio
+from collections import defaultdict
+import weakref
+#from aio_timers import Timer
 
 log = logging.getLogger(__name__)
 
 sockets = set()
+
+async def callback(ws):
+    print("Hello {}!")
+    await ws.send_str("test");
+
+# timer is scheduled here
 
 
 def create_json_response(cmd, payload):
@@ -17,10 +27,31 @@ def create_json_response(cmd, payload):
     return json_d
 
 
+async def timer(app):
+    print('hello')
+    msg = {
+        "cmd": "SENSOR_UPDATE",
+        "payload": {
+            "t": "1",
+            "h": "1"
+        }
+    }
+    count = 0
+    while True:
+        await asyncio.sleep(3)
+        for ws in set(app['websockets']):
+            msg['payload']['t'] = count
+            msg['payload']['h'] = count
+            count += 1
+            print('world')
+            data = json.dumps(msg)
+            await ws.send_str(data)
+
+
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-    sockets.add(ws)
+    request.app['websockets'].add(ws)
     async for msg in ws:
         if msg.type == aiohttp.WSMsgType.TEXT:
             log.info("got %s on socket: %s", msg.data, request.rel_url)
@@ -42,11 +73,21 @@ async def websocket_handler(request):
 
 async def init_app():
     app = web.Application()
-    app['websockets'] = {}
+    app['websockets'] = weakref.WeakSet()
     app.add_routes([web.get('/ws', websocket_handler)])
     app.router.add_static('/', path=str('../static'))
+    app.on_startup.append(start_background_tasks)
+    app.on_cleanup.append(cleanup_background_tasks)
     return app
 
+
+async def start_background_tasks(app):
+    app['websoct_listener'] = asyncio.create_task(timer(app))
+
+
+async def cleanup_background_tasks(app):
+    app['websoct_listener'].cancel()
+    await app['websoct_listener']
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
